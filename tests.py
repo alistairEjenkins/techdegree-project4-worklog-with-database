8caf6a1bd87b00cf8d6211158a4af424cc817165
Alistair <alistairedwardjenkins@gmail.com>
2018-06-26 21:21:36 +0100

    85% coverage

--- tests.py
+++ tests.py
@@ -2,7 +2,7 @@ import unittest
 import re
 import string
 import datetime
-
+import io
 
 from unittest.mock import patch
 from models import Entry
@@ -63,30 +63,74 @@ class WorklogTests(unittest.TestCase):
 	
 		for test in tests:
 			t.add_entry(test)
+			
+	def test_validate_main_menu_choice_a(self):
 	
-	def test_validate_main_menu_choice(self):
+		self.assertTrue(wl.valid_main_menu_choice('a'))
+		
+	def test_validate_main_menu_choice_b(self):
 	
-		for choice in string.ascii_letters:
-			if choice in ['a','b','c']:
-				self.assertTrue(wl.valid_main_menu_choice(choice))
-			else:
-				self.assertFalse(wl.valid_main_menu_choice(choice))
+		self.assertTrue(wl.valid_main_menu_choice('b'))
 	
-	def test_validate_search_menu_choice(self):
+	def test_validate_main_menu_choice_c(self):
 	
-		for choice in string.ascii_letters:
-			if choice in ['a','b','c','d','e','f','g']:
-				self.assertTrue(wl.valid_search_menu_choice(choice))
-			else:
-				self.assertFalse(wl.valid_search_menu_choice(choice))
+		self.assertTrue(wl.valid_main_menu_choice('c'))
+			
+	def test_validate_main_menu_choice_error(self):
 	
-	def test_validate_edit_menu_choice(self):
+		self.assertFalse(wl.valid_main_menu_choice('d'))
 	
-		for choice in string.ascii_letters:
-			if choice in ['a','b','c','d']:
-				self.assertTrue(wl.valid_edit_menu_choice(choice))
-			else:
-				self.assertFalse(wl.valid_edit_menu_choice(choice))
+	def test_validate_search_menu_choice_a(self):
+	
+		self.assertTrue(wl.valid_search_menu_choice('a'))
+		
+	def test_validate_search_menu_choice_b(self):
+	
+		self.assertTrue(wl.valid_search_menu_choice('b'))
+		
+	def test_validate_search_menu_choice_c(self):
+	
+		self.assertTrue(wl.valid_search_menu_choice('c'))
+		
+	def test_validate_search_menu_choice_d(self):
+	
+		self.assertTrue(wl.valid_search_menu_choice('d'))
+		
+	def test_validate_search_menu_choice_e(self):
+	
+		self.assertTrue(wl.valid_search_menu_choice('e'))
+		
+	def test_validate_search_menu_choice_f(self):
+	
+		self.assertTrue(wl.valid_search_menu_choice('f'))
+		
+	def test_validate_search_menu_choice_g(self):
+	
+		self.assertTrue(wl.valid_search_menu_choice('g'))
+		
+	def test_validate_search_menu_choice_error(self):
+	
+		self.assertFalse(wl.valid_search_menu_choice('h'))
+	
+	def test_validate_edit_menu_choice_a(self):
+	
+		self.assertTrue(wl.valid_edit_menu_choice('a'))
+		
+	def test_validate_edit_menu_choice_b(self):
+	
+		self.assertTrue(wl.valid_edit_menu_choice('b'))
+		
+	def test_validate_edit_menu_choice_c(self):
+	
+		self.assertTrue(wl.valid_edit_menu_choice('c'))
+		
+	def test_validate_edit_menu_choice_d(self):
+	
+		self.assertTrue(wl.valid_edit_menu_choice('d'))
+		
+	def test_validate_edit_menu_choice_error(self):
+	
+		self.assertFalse(wl.valid_edit_menu_choice('e'))
 	
 	def test_get_name(self):
 		with unittest.mock.patch('builtins.input', side_effect=['bob']):
@@ -104,9 +148,11 @@ class WorklogTests(unittest.TestCase):
 		with unittest.mock.patch('builtins.input', side_effect=[12]):
 			self.assertEqual(wl.get_time_spent(), 12)
 	
-	def test_get_notes(self):
+	def test_get_notes_with_notes(self):
 		with unittest.mock.patch('builtins.input', side_effect=['notes']):
 			self.assertEqual(wl.get_notes(), 'notes')
+	
+	def test_get_notes_no_notes(self):
 		with unittest.mock.patch('builtins.input', side_effect=['']):
 			self.assertEqual(wl.get_notes(), 'None')
 	
@@ -117,6 +163,22 @@ class WorklogTests(unittest.TestCase):
 		self.assertTrue(wl.validate_length('test title', 'title'))
 		self.assertFalse(wl.validate_length('', 'title'))
 	
+	def test_no_entries(self):
+	
+		self.assertTrue(wl.no_entries([]))
+		self.assertFalse(wl.no_entries(['test_name1']))
+	
+	def test_no_search(self):
+	
+		with unittest.mock.patch('builtins.input', side_effect=['r']):
+			self.assertTrue(wl.no_search('name'))
+	
+	def test_valid_choice(self):
+	
+		self.assertTrue(wl.valid_choice('test name1', ['test name1',
+														 'test name2,',
+														 'test name3']))
+
 	def test_add_entry(self):
 	
 		self.assertEqual(Entry.select().count(), 6)
@@ -129,8 +191,7 @@ class WorklogTests(unittest.TestCase):
 		self.assertEqual(test7['title'], test7_from_db[0].title)
 		self.assertEqual(test7['time_spent'], test7_from_db[0].time_spent)
 		self.assertEqual(test7['notes'], test7_from_db[0].notes)
-		test7_from_db[0].delete_instance()
-		
+	
 	def test_employees(self):
 	
 		# generate a set of all names in test data
@@ -224,8 +285,9 @@ class WorklogTests(unittest.TestCase):
 					dates1.add(date)
 	
 		dates2 = []
-		for entry in t.range_of_dates(start_date, end_date):
-			dates2.append(entry.date)
+		with unittest.mock.patch('builtins.input', side_effect=[start_date, end_date]):
+			for entry in wl.find_from_range_of_dates():
+				dates2.append(entry.date)
 	
 		dates1 = list(dates1)
 	
@@ -270,51 +332,84 @@ class WorklogTests(unittest.TestCase):
 		self.assertEquals(entries1.sort(), entries2.sort())
 	
 	
-	def test_match_exists(self):
-
+	def test_match_exists_true(self):
+	
 		self.assertTrue(wl.match_exists(t.entry_by_name('test '
 														 'name1')))
+	def test_match_exists_false(self):
 		self.assertFalse(wl.match_exists(t.entry_by_name('error')))
 	
-	def test_page_menu(self):
+	def test_page_menu_con1(self):
 	
 		self.assertEqual(wl.page_menu(t.entry_by_name('test name1'), 0), 'edr')
+	
+	def test_page_menu_con2(self):
 		self.assertNotEqual(wl.page_menu(t.entry_by_name('test name1'), 0),
 							'nedr')
+	def test_page_menu_con3(self):
 		self.assertEqual(wl.page_menu(t.entry_by_name('test name2'), 0),
 							'nedr')
-		self.assertNotEqual(wl.page_menu(t.entry_by_name('test name2'), 1),
-							'nedr')
+	def test_page_menu_con4(self):
+		self.assertNotEqual(wl.page_menu(t.entry_by_name('test name2'), 1),'nedr')
+		
+	def test_page_menu_con5(self):
 		self.assertEqual(wl.page_menu(t.entry_by_name('test name3'), 0),
 							'nedr')
+	def test_page_menu_con6(self):
 		self.assertEqual(wl.page_menu(t.entry_by_name('test name3'), 1),
 							'npedr')
+	def test_page_menu_con7(self):	
 		self.assertEqual(wl.page_menu(t.entry_by_name('test name3'), 2),
 						 'pedr')
 	
-	def test_edit_table(self):
+	def test_edit_table_a(self):
 	
 		entry = Entry.select().where(Entry.name == 'test name1')
-		print(entry[0].name)
-		updates = {'a' : '01/01/0101', 'b' : 'aaaaa', 'c': 1, 'd': 'aaaaa'}
-		for selection, new_info in updates.items():
-			t.edit_table(entry[0], selection, new_info)
+		t.edit_table(entry[0], 'a', '01/01/0101')
 	
-		self.assertEqual(entry[0].date, updates['a'])
-		self.assertEqual(entry[0].title, updates['b'])
-		self.assertEqual(entry[0].time_spent, updates['c'])
-		self.assertEqual(entry[0].notes, updates['d'])
+		self.assertEqual(entry[0].date, '01/01/0101')
 	
+	def test_edit_table_b(self):
 	
+		entry = Entry.select().where(Entry.name == 'test name1')
+		t.edit_table(entry[0], 'b', 'aaaaa')
+	
+		self.assertEqual(entry[0].title, 'aaaaa')
+		
+	def test_edit_table_c(self):
+	
+		entry = Entry.select().where(Entry.name == 'test name1')
+		t.edit_table(entry[0], 'c', 1)
+	
+		self.assertEqual(entry[0].time_spent, 1)
+		
+	def test_edit_table_default(self):
+	
+		entry = Entry.select().where(Entry.name == 'test name1')
+		t.edit_table(entry[0], 'd', 'new notes')
+	
+		self.assertEqual(entry[0].notes, 'new notes')
+	
+	def test_edit_further(self):
+		
+		with unittest.mock.patch('builtins.input', side_effect=['y']):
+			self.assertTrue(wl.edit_further())
+			
 	def test_delete(self):
 	
-		#t.add_entry(test7)
 		entries = Entry.select()
-		self.assertEqual(entries.count(), 6)
-		entries[4].delete_instance()
-		self.assertFalse(Entry.select().where(Entry.name == 'test '
-															'name4').exists())
+		with unittest.mock.patch('builtins.input', side_effect=['y']):
+			wl.delete_entry(entries[0])
 		self.assertEqual(entries.count(), 5)
+		
+	def test_valid_date_range(self):
+		
+		self.assertTrue(wl.valid_date_range('12/12/1212','14/12/1212'))
+	
+	def test_confirm_delete(self):
+	
+		self.assertTrue(wl.confirm_delete('y'))
+		self.assertFalse(wl.confirm_delete('n'))
 	
 	def tearDown(self):
 	
